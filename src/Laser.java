8b169327b044e993c2b1a8c3aa11f8e18038735b
Nadizdom34 <ndomingu@macalester.edu>
2024-04-15 15:08:43 -0500

    laser constructor

--- src/Laser.java
+++ src/Laser.java
@@ -2,5 +2,10 @@ import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Rectangle;
 
 public class Laser extends Rectangle{
+
+    public Laser(double x, double y, double width, double height) {
+        super(x, y, width, height);
+        //TODO Auto-generated constructor stub
+    }
     
 }
