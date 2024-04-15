560c9ad1474ff5a8af8ec2a6c02d4420c73548f0
Nadizdom34 <ndomingu@macalester.edu>
2024-04-15 15:20:26 -0500

    creating the laser
    per abby's recommendation the laser should be a line(which is still a graphic thing) .

--- src/Laser.java
+++ src/Laser.java
@@ -1,11 +1,13 @@
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Line;
 import edu.macalester.graphics.Rectangle;
 
-public class Laser extends Rectangle{
+public class Laser extends Line{
 
-    public Laser(double x, double y, double width, double height) {
-        super(x, y, width, height);
-        //TODO Auto-generated constructor stub
+    public Laser(double x1, double y1, double x2, double y2) {
+        super(x1, y2, x2, y2);
+        this.setStrokeWidth(3);
     }
+
     
 }
