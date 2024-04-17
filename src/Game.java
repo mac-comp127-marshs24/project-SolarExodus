b113c9cf0c3bc7f2627d78919b6122dd000b9084
yeshedj <157068258+yeshedj@users.noreply.github.com>
2024-04-17 11:59:44 -0500

    added space bg

--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,26 @@
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Image;
+
 import java.awt.Color;
 import java.util.ArrayList;
 
 public class Game {
     private CanvasWindow canvas;
     private static final int HEIGHT = 800;
-    private static final int WIDTH = 800;
+    private static final int WIDTH = 600;
     private Spaceship spaceship;
     private ArrayList<Laser> lasers;
 
 
     public Game() {
         canvas = new CanvasWindow("GameName", WIDTH, HEIGHT);
-        canvas.setBackground(new Color(10, 0, 20));
+        // canvas.setBackground(new Color(10, 0, 20));
 
-        // PlanetImage spaceBG = new PlanetImage("res/spaceBG.jpeg");
-        // canvas.setBackground(spaceBG);
+        Image spaceBG = new Image("spaceBG.jpeg");
+        spaceBG.setMaxWidth(WIDTH);
+        spaceBG.setMaxHeight(HEIGHT);
+        canvas.add(spaceBG);
+
 
         spaceship = new Spaceship();
         spaceship.addToCanvas(canvas);
@@ -23,6 +28,7 @@ public class Game {
         lasers = new ArrayList <> ();
         createLaser();
     }
+
     public void createLaser(){
         canvas.onCharacterTyped(event-> {
             double x1 = spaceship.getX()+23;
